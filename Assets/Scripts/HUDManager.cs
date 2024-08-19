fd2204461fc43d14634e601b22d143639897a01d
EvanMcRae <evanmmcrae@gmail.com>
2024-08-19 03:04:22 -0400

    Mostly finished credits + other tiny fixes

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HUDManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI EXPValue, WaveValue, LevelValue;
-    [SerializeField] private GameObject WorkshopPanel;
+    [SerializeField] private GameObject WorkshopPanel = null;
     // TODO need hearts container here
+    // TODO need hurt vignette here
 
     // Update is called once per frame
     void Update()
     {
         WaveValue.text = WaveManager.CurrentWave + "";
         LevelValue.text = stageManager.level + "";
+    }
+
+    public void OpenWorkshop()
+    {
+        // TODO stopgap for now, remove later
+        EventSystem.current.SetSelectedGameObject(null);
+        if (WorkshopPanel != null)
+            WorkshopPanel.SetActive(true);
     }
 }
