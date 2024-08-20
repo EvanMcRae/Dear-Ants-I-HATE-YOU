1dcf455802f0e61f7ada5587b53dc67650162e2e
RJWMD <113476572+RJWMD@users.noreply.github.com>
2024-08-19 21:48:32 -0400

    Upgrade screen fully implemented

--- Assets/Scripts/TowerShopListing.cs
+++ Assets/Scripts/TowerShopListing.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerShopListing : ShopListing
 {
@@ -15,6 +16,20 @@ public class TowerShopListing : ShopListing
         clickToSpawnManager.PlacingMode = clickToSpawnManager.PlacingBehaviour.tower;
         clickToSpawnManager.playerTowerChoice = TowerName;
         clickToSpawnManager.currentPlacementCost = cost;
+        backing.color = Color.white;
+        //button.Select();
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (clickToSpawnManager.PlacingMode != clickToSpawnManager.PlacingBehaviour.tower || (clickToSpawnManager.playerTowerChoice != TowerName))
+        {
+            backing.color = new Color(.8f, .8f, .8f);
+            //EventSystem.current.SetSelectedGameObject(null);
+        }
 
     }
 }
