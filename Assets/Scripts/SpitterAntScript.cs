de12540ec98ddca7c76e21eeb887c963c2ea39d7
RJWMD <113476572+RJWMD@users.noreply.github.com>
2024-08-20 09:06:29 -0400

    Spitter ants now have range with their attacks

--- Assets/Scripts/SpitterAntScript.cs
+++ Assets/Scripts/SpitterAntScript.cs
@@ -2,37 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class SpitterAntScript : EnemyAI
 {
-
-    public Path path;
-    public int currPathId = 0;
-
-    public float moveSpeed = 1;
-
-    public int health = 10;
-
-    public float lastAttack;
-    public float attackCooldown;
-    public int attackDamage = 1;
-
-    //resource points awarded when killed
-    public int resourceAward = 1;
-    
-    protected GameplayManager manager;
-
-    public Animator visuals;
-
-    public GameObject DeathExplosion;
-
     // Start is called before the first frame update
     void Start()
     {
-        manager = FindObjectOfType<GameplayManager>();
+        
     }
 
-    // Update is called once per frame
-    protected virtual void Update()
+    protected override void Update()
     {
         if (GameplayManager.quit || GameplayManager.won || GameplayManager.lost || GameplayManager.playingAgain) return;
 
@@ -41,7 +19,7 @@ public class EnemyAI : MonoBehaviour
         {
             //deal damage to the player
             manager.takeDamage();
-            
+
             Destroy(gameObject);
             return;
         }
@@ -51,12 +29,28 @@ public class EnemyAI : MonoBehaviour
         if (nextTile.hasTower && !nextTile.spawnedtower.GetComponent<Tower>().dead)
         {
             //AttackTower
-            if(lastAttack + attackCooldown < Time.time)
+            if (lastAttack + attackCooldown < Time.time)
             {
                 lastAttack = Time.time;
                 nextTile.spawnedtower.GetComponent<Tower>().takeDamage(attackDamage);
             }
             return;
+        }
+
+        if (currPathId + 1 < path.path.Count)
+        {
+            tileScript nextNextTile = path.path[currPathId + 1].GetComponent<tileScript>();
+
+            if (nextNextTile.hasTower && !nextNextTile.spawnedtower.GetComponent<Tower>().dead)
+            {
+                //AttackTower
+                if (lastAttack + attackCooldown < Time.time)
+                {
+                    lastAttack = Time.time;
+                    nextNextTile.spawnedtower.GetComponent<Tower>().takeDamage(attackDamage);
+                }
+                return;
+            }
         }
 
         Vector2 targetPosition = new Vector2(path.path[currPathId].transform.position.x, path.path[currPathId].transform.position.z);
@@ -65,7 +59,7 @@ public class EnemyAI : MonoBehaviour
             currPathId++;
         }
 
-        
+
 
         Vector2 moveDir = (targetPosition - new Vector2(transform.position.x, transform.position.z)).normalized * moveSpeed * Time.deltaTime;
         transform.position += (new Vector3(moveDir.x, 0, moveDir.y));
@@ -80,24 +74,6 @@ public class EnemyAI : MonoBehaviour
         else
             visuals.transform.localScale = new Vector3(-1, 1, 1);
 
-        
-    }
 
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health <= 0)
-            Die();
-    }
-
-    public void Die()
-    {
-        GameplayManager.main.addResource(resourceAward);
-        
-        GameObject addedExplosion = Instantiate(DeathExplosion, transform.position, Quaternion.Euler(90, 0, 0));
-        addedExplosion.transform.localScale = Vector3.one * Random.Range(.2f, .3f);
-        Destroy(gameObject);
     }
 }
-    
